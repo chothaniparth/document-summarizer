a649bb169690f0d32c8893c96557bbdfce32c31a
parth chothani <parthchothani560@gmail.com>
2025-11-16 19:21:55 +0530

    Done async workers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Query
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 import os
@@ -10,8 +10,24 @@ from langchain_qdrant import QdrantVectorStore
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from openai import OpenAI
+from motor.motor_asyncio import AsyncIOMotorClient
+import uvicorn
+from clients.rq_client import queue
+from queues.worker import process_query
+from passlib.context import CryptContext
+import jwt
+import datetime
 
 load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+JWT_SECRET = os.getenv('JWT_SECRET') 
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[MONGO_DB_NAME]
+
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
 
@@ -26,6 +42,13 @@ embedding_model = OpenAIEmbeddings(
     model="text-embedding-3-large"
 )
 
+def main():
+    uvicorn.run(app, port=8000, host="0.0.0.0")
+
+@app.get('/')
+def startServer():
+    return {"status" : "server is running"}
+ 
 @app.post("/save-file/")
 async def save_file(file: UploadFile = File(...), UserId: str = 'default', DocumentId: str = 'default'):
     # save file
@@ -68,37 +91,52 @@ async def save_file(file: UploadFile = File(...), UserId: str = 'default', Docum
     
 class QueryBody(BaseModel):
     query: str
+    
+class UserSignup(BaseModel):
+    name: str
+    email: str
+    password: str
+    
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+@app.get("/getResult")
+async def getResult(job_id: str = Query(..., description="job_id")):
+    job = queue.fetch_job(job_id)
+    print(f"\n\n {job.id} \n\n")
+    return job.return_value()
 
 @app.post("/query")
 async def userQnA(body : QueryBody):
-    
-    vector_db = QdrantVectorStore.from_existing_collection(
-        embedding = embedding_model,
-        url = "http://localhost:6333",
-        collection_name = "learning_rag"
-    )
+    job = queue.enqueue(process_query, body.query)
+    return {"status" : "query queued", "job_id" : job.id}
+    # vector_db = QdrantVectorStore.from_existing_collection(
+    #     embedding = embedding_model,
+    #     url = "http://localhost:6333",
+    #     collection_name = "learning_rag"
+    # )
 
-    search_result = vector_db.similarity_search(query=body.query)
+    # search_result = vector_db.similarity_search(query=body.query)
     
-    context = "\n\n\n".join({f"Page Content: {result.page_content}\nPage Number:{result.metadata["page_label"]}\nfile location: {result.metadata["source"]}" for result in search_result})
+    # context = "\n\n\n".join({f"Page Content: {result.page_content}\nPage Number:{result.metadata["page_label"]}\nfile location: {result.metadata["source"]}" for result in search_result})
     
-    SYSTEM_PROMPT = f"""You are a help full assistant who answers user query based on the available context retrieved from a PDF file along with the page contents and page number.
+    # SYSTEM_PROMPT = f"""You are a help full assistant who answers user query based on the available context retrieved from a PDF file along with the page contents and page number.
     
-    you should only answer based on the user based on the followingcontext and nevigate the user to open the right page number to know more. 
+    # you should only answer based on the user based on the followingcontext and nevigate the user to open the right page number to know more. 
 
-    Context: 
-    {context}
-    """
+    # Context: 
+    # {context}
+    # """
     
-    response = openai_client.chat.completions.create(
-        model="gpt-5",
-        messages=[
-            {"role" : "system", "content" : SYSTEM_PROMPT},
-            {"role" : "user", "content" : body.query}
-        ]
-    )
-
-    return {"result" : response.choices[0].message.content}
+    # response = openai_client.chat.completions.create(
+    #     model="gpt-5",
+    #     messages=[
+    #         {"role" : "system", "content" : SYSTEM_PROMPT},
+    #         {"role" : "user", "content" : body.query}
+    #     ]
+    # )
+    
 
 # @app.get("/read-file/")
 # async def read_file(filename: str):
@@ -111,3 +149,38 @@ async def userQnA(body : QueryBody):
 #         content = f.read()
 
 #     return {"filename": filename, "content": content.decode(errors="ignore")}
+
+@app.post("/signup")
+async def signup(body: UserSignup):
+    
+    user = await db.users.find_one({"email": body.email})
+    if user:
+        return {"error": "Email already exists"}
+
+    hashed = pwd.hash(body.password)
+
+    res = await db.users.insert_one({
+        "name": body.name,
+        "email": body.email,
+        "password": hashed
+    })
+
+    return {"status": "user created", "id": str(res.inserted_id)}
+
+@app.post("/login")
+async def login(body: UserLogin):
+    user = await db.users.find_one({"email": body.email})
+    if not user:
+        return {"error": "Invalid credentials"}
+
+    if not pwd.verify(body.password, user["password"]):
+        return {"error": "Invalid credentials"}
+
+    token = jwt.encode({
+        "user_id": str(user["_id"]),
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+    }, JWT_SECRET)
+
+    return {"token": token}
+
+main()
